d21f011a8e84f576c9de980abfbe0081c084505f
Ian Stewart <iacostewart@msn.com>
2018-01-24 11:36:34 -0500

    everyting Works

---
+++ MYmain.js
@@ -0,0 +1,50 @@
+$(() => {
+
+
+// //    $.ajax({
+//         type: 'GET',
+//         url: 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
+//    }).done((ron) =>{
+//        let wisdom = ron;
+//        console.log(wisdom);
+//    })
+// // 
+   
+
+
+localStorage.clear()
+let quote = [] 
+
+      $('#toggle').click(() =>{
+          $('#here').empty()
+        $.ajax({
+            type: 'GET',
+            url: 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
+        }).done((ron) => {
+            console.log(ron);
+                let wisdom = $(`<h2>${ron}</h2>`)
+                $('#here').append(wisdom)
+          
+     
+            })
+
+         
+    
+         
+
+       
+            
+            })
+        });   
+
+/*$( "#target" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+Now if we click on this element, the alert is displayed:
+ */   
+    
+    
+        
+           
+     
+    
